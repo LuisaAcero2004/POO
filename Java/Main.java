687623baaf7e84261c475c3ab5a65b4077f2a714
LuisaAcero2004 <luisafernanda_2004@msn.com>
2022-07-12 20:46:02 -0500

    Create Car objects in java and python

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,17 @@
 class Main {
 
     public static void main(String[] args) {
-        
+        //Create new object
+        Car car = new Car();
+        car.license = "FIS629";
+        car.driver = "Luisa Acero";
+        car.passengers = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "ZXC956";
+        car2.driver = "Sandra Martinez";
+        car2.passengers = 3;
+        car2.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,13 @@
 from pip import main
-
+from car import Car
 
 if __name__ =="__main__":
-    print("Hola Mundo")
+    car = Car()
+    car.license = "ASD960"
+    car.driver = "Luisa Acero"
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "ZXC096"
+    car2.driver = "Sandra Martinez"
+    print(vars(car2))
