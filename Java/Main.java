2225593f1b23c8f7bd999d4075bfda53a2127fa5
LuisaAcero2004 <luisafernanda_2004@msn.com>
2022-07-20 18:50:49 -0500

    Add encapsulation example in java

--- Java/Main.java
+++ Java/Main.java
@@ -4,13 +4,13 @@ class Main {
         //Create new object
 
         Driver driver = new Driver("Luisa Acero",1234567890);
-        UberX car = new UberX("FIS629" , driver,"Chevrolet","Aveo");
-        car.passengers = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("FIS629" , driver,"Chevrolet","Aveo");
+        uberX.setPassengers(1);
+        uberX.printDataCar();
 
         Driver driver2 = new Driver("Sandra Martinez", 789456125);
-        UberPool car2 = new UberPool("ZXC956", driver2,"Hyundai","i5");
-        car2.passengers = 3;
-        car2.printDataCar();
+        UberPool uberPool = new UberPool("ZXC956", driver2,"Hyundai","i5");
+        uberPool.printDataCar();
+
     }
 }
