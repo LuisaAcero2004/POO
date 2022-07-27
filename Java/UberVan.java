cc1a494457ae7b3cf886a547ff62a2542415ac10
LuisaAcero2004 <luisafernanda_2004@msn.com>
2022-07-26 21:05:49 -0500

    Adding polymorphism in java

--- Java/UberVan.java
+++ Java/UberVan.java
@@ -5,6 +5,7 @@ public class UberVan extends Car{
 
     Map<String, Map<String,Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
+    private Integer passengers;
 
 
     public UberVan(String license, Account driver, Map<String, Map<String,Integer>> typeCarAccepted,ArrayList<String> seatsMaterial ){
@@ -12,6 +13,16 @@ public class UberVan extends Car{
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
 
+    }
+
+    @Override
+    public void setPassengers(Integer passengers){
+        if(passengers == 6){
+            this.passengers = passengers;
+        }else{
+            System.out.println("The value of passengers should be 4");
+        }
+        
     }
     
 }
